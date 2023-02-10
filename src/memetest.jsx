223ec3569a8ab2598ef0363add5f42c75926217d
crpana <panacarlosramon@gmail.com>
2023-02-10 15:26:00 -0300

    finish

--- src/memetest.jsx
+++ src/memetest.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MEMES = [
     'https://i.kym-cdn.com/entries/icons/original/000/017/618/pepefroggie.jpg',
@@ -12,7 +12,7 @@ const MEMES = [
     'https://i.kym-cdn.com/entries/icons/original/000/026/452/928d8a9a57515320068a2c339e88fd28.jpg',
     'https://i.kym-cdn.com/photos/images/newsfeed/001/218/292/7e6.png'
 
-].flatMap((m) => [m, m]).sort(() => Math.random() - 0.5);
+].flatMap((m) => [`a|${m}`, `b|${m}`]).sort(() => Math.random() - 0.5);
 
 
 
@@ -20,7 +20,27 @@ export default function Memetest() {
     console.log(MEMES);
 
     const [correct, setCorrect] = useState([]);
-    const [select, setSelect] = useState([]);
+    const [selected, setSelected] = useState([]);
+
+    useEffect(() => {
+        if (selected.length === 2) {
+            if (selected[0].split("|")[1] === selected[1].split("|")[1]) {
+                setCorrect((correct) => correct.concat(selected));
+            }
+            setTimeout(() => setSelected([]), 1000)
+        }
+
+
+
+
+    }, [selected]);
+
+    useEffect(()=>{
+        if(correct.length===MEMES.length){
+            alert("ganaste bobis")
+            location.reload()
+        }
+    },[correct])
 
     return (
         <ul style={{
@@ -28,14 +48,24 @@ export default function Memetest() {
             gridTemplateColumns: 'repeat(auto-fill,minmax(128px,1fr))',
             gap: 24
         }}>
-            {
-                MEMES.map(image =>
-                    <li key={image} style={{ padding: 12, border: '1px solid #555' }}>
-                        <img src={image} alt='memes' key={image} />
+
+            {MEMES.map((image) => {
+                const [, url] = image.split("|");
+                console.log(url, 'esta es la url ');
+                return (
+                    <li
+                        onClick={() => selected.length < 2 && setSelected(selected => selected.concat(image))}
+                        key={image}
+                        style={{ cursor: 'pointer', padding: 12, border: '1px solid #555' }}>
+                        {selected.includes(image) || correct.includes(image) ?
+                            <img src={url} alt='memes' key={image} /> :
+                            <img key={image} src='https://icongr.am/clarity/ban.svg?size=128&color=currentColor' alt='memeso' />
+
+                        }
                     </li>
-
-
-                )
+                );
+            }
+            )
             }
 
         </ul>
